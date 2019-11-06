d7ff24387ce7501b2910ff4c150157191ee4a94b
Sveinbjörn Guðjohnsen <s182350@student.dtu.dk>
2019-11-06 15:42:27 +0100

    Add syntax highlighting to webui

--- src/WebApplication/JSInterop/webpack.config.js
+++ src/WebApplication/JSInterop/webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
             }
         ]
     },
